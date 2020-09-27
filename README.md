19f0b25bc64936e414cbcf6b80c0f4aaa152ac7a
andressatheotonio <andressatheotonio@gmail.com>
2020-09-27 11:44:32 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-etm4m7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-etm4m7)
---
+++ src/shared/model/usuario.ts
@@ -0,0 +1,7 @@
+export class Usuario {
+  nome: string;
+  matricula: string;
+  curso: string;
+  campus: string;
+
+}
